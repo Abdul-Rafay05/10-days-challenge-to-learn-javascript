f306e8387b1545ec8da5d9dc9dc2bdbaa27d353f
Abdul-Rafay05 <syedabdulrafay163@gmail.com>
2025-01-21 03:24:22 +0500

    10 Days Challenge to learn JS

---
+++ day no 1/index.js
@@ -0,0 +1,24 @@
+
+// Day 1: JavaScript Basics
+
+// Create a program to perform basic mathematical operations like addition or subtraction using user inputs.
+// let user_input1 = 10
+// let user_input2 = 20
+
+// console.log(`${user_input1} + ${user_input2} = ${user_input1 + user_input2}`);
+// console.log(`${user_input1} x ${user_input2} = ${user_input1 * user_input2}`);
+// console.log(`${user_input1} / ${user_input2} = ${user_input1 / user_input2}`);
+// console.log(`${user_input1} - ${user_input2} = ${user_input1 - user_input2}`);
+// console.log(`${user_input1} % ${user_input2} = ${user_input1 % user_input2}`);
+
+
+// Write a script to ask the user for their name and display a personalized greeting in the console.
+
+// let user_name = "Muhammad Nabeen"
+
+// console.log(`Hi, Greeting ${user_name}.`);
+
+let year = 2003;
+let result = year == 2003 ? "Rafay was born in 2003" : "Rafay wasn't born in 2003"
+console.log(result);
+
