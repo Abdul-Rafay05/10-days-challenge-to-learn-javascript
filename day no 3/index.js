4c754e5e079742498761635c0f5c05ad333caa92
Abdul-Rafay05 <syedabdulrafay163@gmail.com>
2025-01-22 15:12:30 +0500

    challenge no 3

---
+++ day no 3/index.js
@@ -0,0 +1,27 @@
+// Practice Tasks
+// Write a function that calculates the square of a number and returns the result.
+// function CalSq(a) {
+//     return a*a
+// }
+// let number = 78
+// let result = CalSq(number)
+// console.log(result);
+
+// Create a function to check whether a given string is a palindrome (same forwards and backward).
+
+// function isPalindrome(a) {
+//     if (a === a.split("").reverse().join("")) {
+//         return true
+//     } else {
+//         false
+//     }
+// }
+// let userInput = "refer"
+// console.log(userInput);
+
+// let result = isPalindrome(userInput)
+// if (result) {
+//     console.log("it’s a palindrome");
+// } else {
+//     console.log("it’s not a palindrome");
+// }
